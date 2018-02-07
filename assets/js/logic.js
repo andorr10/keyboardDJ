3853e885c4d997c2423c535fa559d66e67f1ba4a
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2018-02-07 14:59:48 -0600

    html done

---
+++ assets/js/logic.js
@@ -0,0 +1,14 @@
+
+
+var count = 2;
+
+function changeBackground(){
+
+if (count % 2 =0){
+document.body.style.backgroundImage = "url('img_tree.png')";
+}
+else{
+
+document.body.style.backgroundImage = "url('img_tree.png')";
+}
+}
